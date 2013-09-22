88458868fc973232f8914059cca470b67c18d5df
LenyeMeng <lenyemeng@anjukeinc.com>
2013-09-23 06:56:01 +0800

    Initial Commit

---
+++ security/LSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  LSAppDelegate.h
+//  security
+//
+//  Created by 孟 智 on 13-9-23.
+//  Copyright (c) 2013年 孟 智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
